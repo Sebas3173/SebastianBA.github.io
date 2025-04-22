a263594bd4835a079a842d5f8919ba3945779ecb
Sebas3173 <139717515+Sebas3173@users.noreply.github.com>
2025-04-22 16:00:47 -0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,7 @@ import icon from "astro-icon";
 // https://astro.build/config
 export default defineConfig({
   site: "https://sebastianba.github.io",
+  base: '/SebastianBA.github.io/',
   integrations: [preact(), icon(), sitemap({
     filter: (page) =>
       !page.includes("/blog/tags") &&
